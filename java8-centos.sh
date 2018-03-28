147ac8c7e84055ba49d557bd9b0ae7a2be2f4cfb
PravinMagdum <pravin.magdum@gmail.com>
2018-03-29 00:09:45 +0530

    Update java8-centos.sh

--- java8-centos.sh
+++ java8-centos.sh
@@ -3,3 +3,4 @@ yum install wget -y
 cd /opt/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz"
 tar xzf jdk-8u161-linux-x64.tar.gz
+//https://tecadmin.net/install-java-8-on-centos-rhel-and-fedora/
